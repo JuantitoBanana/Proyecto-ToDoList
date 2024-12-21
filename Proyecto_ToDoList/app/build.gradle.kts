4d4fa57f86fefd6738186b6db4f0526a988f937e
JuanmaCastanios <juanjoseblancodiaz1504@gmail.com>
2024-12-21 23:43:19 +0100

    Creacion funcionalidad BD

--- Proyecto_ToDoList/app/build.gradle.kts
+++ Proyecto_ToDoList/app/build.gradle.kts
@@ -46,9 +46,7 @@ dependencies {
     val room_version = "2.6.1"
     implementation("androidx.room:room-runtime:$room_version")
     annotationProcessor("androidx.room:room-compiler:$room_version")
-    // To use Kotlin annotation processing tool (kapt)
     kapt("androidx.room:room-compiler:$room_version")
-    // To use Kotlin Symbol Processing (KSP)
     ksp("androidx.room:room-compiler:$room_version")
 
     implementation(libs.androidx.core.ktx)
